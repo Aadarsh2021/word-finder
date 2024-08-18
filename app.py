631a1daa67c8a0c983ec5cf35fc5aeb07d045e9f
Aadarsh Thakur <125186469+Aadarsh2021@users.noreply.github.com>
2024-08-18 15:23:03 +0530

    c

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for
 from itertools import permutations
 import nltk
-
-# Download NLTK words corpus if not already downloaded
-nltk.download('words')
 from nltk.corpus import words
 
 app = Flask(__name__)
 
 # Load the list of valid words
+nltk.download('words')
 dictionary = set(word.lower() for word in words.words())
 
 @app.route("/", methods=["GET", "POST"])
@@ -24,11 +22,11 @@ def letters_input(num_letters):
     if request.method == "POST":
         letters = request.form.getlist("letters")
         letters = ''.join(letters).lower()
-        if len(letters) == num_letters:
+        if len(letters) >= num_letters:  # Check if the input is at least the number of letters required
             valid_words = find_words(letters)
-            return render_template("index.html", words=valid_words, letters=letters)
+            return render_template("result.html", words=valid_words, letters=letters, num_letters=num_letters)
         else:
-            error = "Please fill all the letters correctly."
+            error = "The number of letters provided is less than the number of letters required."
             return render_template("letters_input.html", num_letters=num_letters, error=error)
     return render_template("letters_input.html", num_letters=num_letters)
 
@@ -37,10 +35,9 @@ def find_words(letters):
     for i in range(1, len(letters) + 1):
         all_permutations.update([''.join(p) for p in permutations(letters, i)])
     valid_words = {word: len(word) for word in all_permutations if word in dictionary}
-    
-    # Sort words first by length and then alphabetically
-    sorted_words = dict(sorted(valid_words.items(), key=lambda item: (item[1], item[0])))
-    return sorted_words
+    # Sort the valid words by their length
+    sorted_valid_words = dict(sorted(valid_words.items(), key=lambda item: item[1]))
+    return sorted_valid_words
 
 if __name__ == "__main__":
     app.run(debug=True)
